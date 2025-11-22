385766b9f253e235ac931454c4bc3b0e6f0877af
Navya2208 <sharma.navya228@gmail.com>
2025-11-22 11:25:16 +0530

    Create FINAL.py
    organizing workflow

---
+++ FINAL.py
@@ -0,0 +1,17 @@
+"""
+Firm Resource Allocation Optimization (ECOS-forced)
+Team: Cute Force
+Members: Potini Sahiti, Navya Sharma, D. Sreeteja
+"""
+#____________________ECOS CHECK___________________________________
+
+
+#____________QUIZ & MAPPING QUIZ PARAMETERS_______________________
+
+#_________________KKT & KKT RESULT DISPLAYING_____________________
+
+#_________________DIAGNOSTICS & OPTIMISATION______________________
+
+#__________________DISPLAYING RESULT______________________________
+
+#_____________________MAIN FUNCTION_______________________________
